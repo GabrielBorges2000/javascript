8bae5ba17366567a1a87396804fde90001f5b435
Gabriel_Borges <gabriel.borgesoliveira.7@hotmail.com>
2023-01-04 08:34:23 -0300

    continuação...

--- DOM/getAttribute.js
+++ DOM/getAttribute.js
@@ -4,6 +4,14 @@
 function input() {
     const input = document.querySelector('input');
 
-    console.log(input.getAttribute('placeholder'))
+    console.log(input.getAttribute('placeholder')) //Pega o valor dentro do meu elemento
+    
+    if(input.hasAttribute('placeholder')) { // hasAttribute verifica se existe essa propriedade
+        console.log('true')
+    } else {
+        console.log(false)
+    }
 
+    input.setAttribute('placeholder', 'placeholder teste') // Adiciona uma propriedade ao meu elemento e o valor da propriedade
+    
 }
